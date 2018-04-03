f43d58b7e19d93864144d6e722592bd16dd4e0e1
xfan0516 <306350948@qq.com>
2018-04-03 08:32:30 +0800

    render 插入表达式“

--- src/index.js
+++ src/index.js
@@ -2,19 +2,22 @@ import React, {Component} from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 
-/* class Header extends Component {
+class Header extends Component {
     render (){
+        const isGoodWord = true;
+        const goodWord = <strong>is good</strong>
+        const baddWord = <strong>is not good</strong>
         return (
-                <h1>React 小书</h1>
+            <div>
+            <h1>React 小书</h1>
+                {isGoodWord ? goodWord : baddWord}
+            </div>
             
         )
     }
-} */
-function renderContent(content){
-    ReactDOM.render(
-        <h1>{content}</h1>,
-        document.getElementById("root")
-    )
-    
 }
-renderContent('hello world')
+
+ReactDOM.render(
+    <Header />,
+    document.getElementById("root")
+)
