38ff99336081da7a72882634d88e97c38a114f8b
xfan0516 <306350948@qq.com>
2018-04-09 08:52:34 +0800

    '不能摸得狗二'

--- src/index.js
+++ src/index.js
@@ -2,45 +2,46 @@ import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 
-class LikeButton extends Component {
-    constructor(){
-        super();
-        this.state = {
-            isLike: false
-        }
+class Dog extends Component {
+    constructor () {
+      super()
+      /* TODO */
+      this.state = {
+        isBarking: false,
+        isRunning: false
+      }
     }
-    /* 
-        handleClickOnLikeButton () {
-            this.setState({ count: 0 }) // => this.state.count 还是 undefined
-            this.setState({ count: this.state.count + 1}) // => undefined + 1 = NaN
-            this.setState({ count: this.state.count + 2}) // => NaN + 2 = NaN
-        }
-    */
-    // setState的第二种使用方式
-    handleClickOnLikeButton(){
-        this.setState((prveState) => {
-            return {count : 0}
-        })
-        this.setState((prveState) => {
-            return {count : prveState.count + 2} // 上一个 setState 的返回是 count 为 0，当前返回 1
-        })
-        this.setState((prveState) => {
-            return {count : prveState.count + 1}  // 上一个 setState 的返回是 count 为 1，当前返回 3
-
-        })
-        console.log(this.state.count)
+    
+    bark () {
+      /* TODO */
+      console.log("isBarking")
+      this.setState({
+        isBarking: true
+      })
+      this.run()
     }
-    render(){
-        return (
-            <button onClick={this.handleClickOnLikeButton.bind(this)}>
-                {this.state.isLike ? '取消': '点赞'}👍
-            </button>
-        )
+    
+    run () {
+      /* TODO */
+      this.setState({
+        isBarking: false,
+        isRunning: true
+      })
+      console.log("isRunning")
+      setTimeout(()=>{
+        this.setState({
+            isRunning: false
+          })
+      },20)
     }
-}
+   
+    render () {
+      return (<div onClick={this.bark.bind(this)}>DOG</div>)
+    }
+  }
 ReactDOM.render(
     <div>
-        <LikeButton />
+        <Dog />
     </div>
     ,
     document.getElementById("root")
