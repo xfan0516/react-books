1ecc36cbc9ffa259dcd23001afc0a98221bb91fe
xfan0516 <306350948@qq.com>
2018-04-09 08:21:35 +0800

    'setState的第二种使用方式'

--- src/index.js
+++ src/index.js
@@ -9,13 +9,26 @@ class LikeButton extends Component {
             isLike: false
         }
     }
-    // setState 接受函数参数
+    /* 
+        handleClickOnLikeButton () {
+            this.setState({ count: 0 }) // => this.state.count 还是 undefined
+            this.setState({ count: this.state.count + 1}) // => undefined + 1 = NaN
+            this.setState({ count: this.state.count + 2}) // => NaN + 2 = NaN
+        }
+    */
+    // setState的第二种使用方式
     handleClickOnLikeButton(){
-        console.log(this.state.isLike)
-        this.setState({
-            isLike: !this.state.isLike
+        this.setState((prveState) => {
+            return {count : 0}
         })
-        console.log(this.state.isLike)
+        this.setState((prveState) => {
+            return {count : prveState.count + 2} // 上一个 setState 的返回是 count 为 0，当前返回 1
+        })
+        this.setState((prveState) => {
+            return {count : prveState.count + 1}  // 上一个 setState 的返回是 count 为 1，当前返回 3
+
+        })
+        console.log(this.state.count)
     }
     render(){
         return (
