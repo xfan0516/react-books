8bde40bb2aaa6103105698c3e9fc0686915ab219
xfan0516 <306350948@qq.com>
2018-04-13 08:24:49 +0800

    '函数式组件'

--- src/index.js
+++ src/index.js
@@ -2,37 +2,37 @@ import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 
-class Computer extends Component {
-  constructor(){
-    super();
-    this.state = {
-      status : 'off' 
-    }
+/**
+ * state vs props
+ * state 和 props 有着千丝万缕的关系。它们都可以决定组件的行为和显示形态。一个组件的 state 中的数据可以通过 props 传给子组件，一个组件可以使用外部传入的 props 来初始化自己的 state。但是它们的职责其实非常明晰分明：state 是让组件控制自己的状态，props 是让外部对组件自己进行配置。
+ * react推荐健使用无状态组件
+ * 
+ */
+class HelloWorld extends Component {
+  constructor() {
+    super()
   }
-  hanleOnClickScreen(){
-    this.setState({
-      status: this.state.status  === 'off' ? 'no' : 'off'
-    })
+
+  sayHi () {
+    alert('Hello World')
   }
+
   render () {
     return (
-      <div>
-        <button onClick={this.hanleOnClickScreen.bind(this)}>开关</button>
-        <Screen showContent={this.state.status  === 'no' ? '显示器亮了' : '显示器关了'} />
-      </div>
+      <div onClick={this.sayHi.bind(this)}>Hello World</div>
     )
   }
 }
 
-class Screen extends Component {
-  static defaultProps = {
-    showContent : '无内容'
-  }
-  render () {
-    return (
-      <div className='screen'>{this.props.showContent}</div>
-    )
-  }
+/**
+ * 用函数式组件的编写方式就是：
+ * @param {*} props 
+ */
+const HelloWorld = (props) => {
+  const sayHi = (event) => alert('Hello World')
+  return (
+    <div onClick={sayHi}>Hello World</div>
+  )
 }
 ReactDOM.render(
   <div>
